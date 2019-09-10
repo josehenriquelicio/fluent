62be7876988658f8d2e02486de5a841086271ab8
JH <jose.licio@b2wdigital.com>
2019-09-10 16:50:33 -0300

    feat: separando por action e brand

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 function hello() {
-    console.log('{action:"insert",payload:"data"}');
+    console.log('{"action":"insert","brand":"americanas","payload":"data"}');
     setTimeout(hello, 2000);
 }
 
